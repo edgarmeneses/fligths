447c49178a4c0b20fcebb87f08262835d0f5e9e2
edgarmenesesc7@gmail.com <edgarmenesesc7@gmail.com>
2023-02-28 16:47:02 -0500

    feat: implemented method read and add documentation in that method

--- src/co/edu/uptc/fligths/service/FIleOperations.java
+++ src/co/edu/uptc/fligths/service/FIleOperations.java
@@ -1,5 +1,6 @@
 package co.edu.uptc.fligths.service;
 
 public interface FIleOperations {
-	void write(String path, String info);
+	void write(String path, Object info);
+	String read(String path);
 }
--- src/co/edu/uptc/fligths/service/impl/FileOperationsImpl.java
+++ src/co/edu/uptc/fligths/service/impl/FileOperationsImpl.java
@@ -1,28 +1,90 @@
 package co.edu.uptc.fligths.service.impl;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 import co.edu.uptc.fligths.service.FIleOperations;
-
+/**
+ * Clase que usa la interface FileOperations
+ * En esta clase se implementan los metodos read y write que permiten leer y escribir
+   un fichero de texto
+ * @version 1.0.0
+ * @author Edgar Antonio Meneses Cadena
+ *
+ */
 public class FileOperationsImpl implements FIleOperations{
 
+	/**
+	 * Metodo que permite escribir una linea en un fiechero de texto
+	 * @param path indica la ruta donde se va a almacenar el archivo ej:(src/resources/file.txt)
+	 * @param info es el objecto o dato que se va a almacenar en el archivo
+	   El param info puede ser un objecto como la clase Arplane, Customer e incluso un dato 
+	   primitivo como un int o un String y se apoya del metodo toString de la clase u objeto
+	   para guardar la linea en el archivo
+	   
+	   Cuando se esten guardando informacion en el archivos se recomienda usar la estrategia 
+	   implementada en los archivos CSV es decir separar cada atributo de la clase por comas,
+	   punto y comas o el delimitador de su preferencia. 
+	   
+	 * En caso de no encontrar la ruta o si el archivo ya esta abiero se genera una excepcion
+	   del tipo IoException. Si el archivo no existe en la ruta se crea atomaticamente
+	 */
 	@Override
-	public void write(String path, String info) {
+	public void write(String path, Object info) {
+		// Se usa la clase java.io.File para cargar el archivo que se va a leer o escribir
 		File file = new File(path);
 	
 		try {
+			// Cuando se quiere escribir en un archivo se usa la clase FileWrite que permite
+			// escribir datos en forma de caracteres en un archivo
+			// El constructor de la clase FileWriter requiere de dos parametros
+			// El primer parametro es el archivo sobre el cual se va ha escribir la informacion
+			// El segundo parametro indica si se quiere borrar y sobreescribir todo el archivo o no
+			// El valor por default para el parametro es false y cuando este esta seteado en dicho
+			// Valor el archivo se sobre escribe completamente
 			FileWriter fileWriter = new FileWriter(file, true);
+			// Para el manejo de archivos de texto se pueden usar dos clases que complementan
+			// y mejoran el performance al momento de bajar datos a disco
+			// Estas clases son BufferWriter y PrintWriter
+			// COn PrintWritter podes imprimir en el archivo un buffer de datos 
+			// Tiene metodos como el write, el append que permiten bajar la informacion al archivo
+			// Pero tambien se puede hacer uso del metodo print y println
+			// Con print simplemente se pinta la informacion en el archivo
+			// Con println se pinta y ademas se realiza un salto de linea en el archivo
 			PrintWriter printWriter = new PrintWriter(fileWriter);
-			
-			printWriter.append(info).append("\n");
+			printWriter.println(info.toString());
+			//printWriter.append(info.toString()).append("\n");
 			printWriter.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	/***
+	 * Metodo que permite leer un archivo
+	 * @param path es la ruta donde se encuentra el archivo que se quiere leer
+	 *
+	 */
+	@Override
+	public String read(String path) {
+		File file = new File(path);
+		StringBuilder builder = new StringBuilder();
+		try (Scanner scanner = new Scanner(file)) {
+			scanner.useDelimiter("\n");
+			
+			while (scanner.hasNext()) {
+				builder.append(scanner.next()).append("\n");
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return builder.toString();
 	}
 
 }
